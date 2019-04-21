3600eee654f2e6a8d30e877339bf80ca8c022c86
arashrestart <arashguest1@gmail.com>
2019-04-21 00:32:11 -0400

    Updates

---
+++ precache-manifest.73f246704613d2df50dc6e306ff7167a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f831d37b20a59de72c7",
+    "url": "/robofriendsPWA/static/js/runtime~main.f8f2cd9f.js"
+  },
+  {
+    "revision": "b6ade3ee10d1ba643618",
+    "url": "/robofriendsPWA/static/js/main.bd69e01f.chunk.js"
+  },
+  {
+    "revision": "00392b7fec1ab1cc7e77",
+    "url": "/robofriendsPWA/static/js/2.d9f107b1.chunk.js"
+  },
+  {
+    "revision": "b6ade3ee10d1ba643618",
+    "url": "/robofriendsPWA/static/css/main.995a9bfa.chunk.css"
+  },
+  {
+    "revision": "00392b7fec1ab1cc7e77",
+    "url": "/robofriendsPWA/static/css/2.0d34da1c.chunk.css"
+  },
+  {
+    "revision": "e221c13a6910ecb354e063484fe636a8",
+    "url": "/robofriendsPWA/index.html"
+  }
+];
